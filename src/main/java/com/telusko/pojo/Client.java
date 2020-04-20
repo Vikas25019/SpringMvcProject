bf8cf23c8494aa6851006842ba841603b901aab5
Vikas25019 <35371508+Vikas25019@users.noreply.github.com>
2020-04-20 08:47:31 -0500

    Add files via upload

--- src/main/java/com/telusko/pojo/Client.java
+++ src/main/java/com/telusko/pojo/Client.java
@@ -3,8 +3,8 @@ package com.telusko.pojo;
 import java.util.LinkedHashMap;
 
 public class Client extends Person {
+
     private String address;
-    private LinkedHashMap<String,String> map = new LinkedHashMap<>();
 
     public String getAddress() {
         return address;
@@ -12,13 +12,6 @@ public class Client extends Person {
 
     public void setAddress(String address) {
         this.address = address;
-    }
-
-    public LinkedHashMap<String,String> clientData() {
-        map.put("clientId",getId());
-        map.put("name",getName());
-        map.put("address",getAddress());
-        return map;
     }
 
 }
